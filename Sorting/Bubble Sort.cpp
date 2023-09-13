1b4a072517f1833ed879ba3b2906398f3d30dec7
Deviprasad-Karkala <deviprasad.karla@gmail.com>
2023-09-13 12:58:54 +0530

    Added some comments.

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -1,27 +1,37 @@
-// Bubble sort = select Max and swap
+// Bubble Sort= push max value to last by adjacent swap
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int i,j,n,temp;
-    cin>>n;
+int main()
+{
+    int i, j, n;
+    cin >> n;
     int a[n];
-    for(i=0;i<n;i++){
-        cin>>a[i];
+    // array input
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
 
-    for(i=n-1;i>=1;i--){
-        for(j=0;j<=i-1;j++){
-            if(a[j]>a[j+1]){
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
+    // Main Logic
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 0; j <= i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                // swap
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
             }
         }
     }
 
-    for(i=0;i<n;i++){
-        cout<<a[i]<<" ";
+    // array output
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
     }
 }
