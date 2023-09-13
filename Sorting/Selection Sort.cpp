d64317934738ea37ff625963896730e8eebca8ea
Deviprasad-Karkala <deviprasad.karla@gmail.com>
2023-09-13 12:32:53 +0530

    Added some comments.

--- Sorting/Selection Sort.cpp
+++ Sorting/Selection Sort.cpp
@@ -1,37 +1,33 @@
-// Selection Sort = Select and Swap
+// Selection Sort = Select Min and Swap
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int i, j, n, min, temp;
-    cout << "Enter the size of an array" << endl;
-    cin >> n;
+int main(){
+    int i,j,n;
+    cin>>n;
     int a[n];
-    for (i = 0; i < n; i++)
-    {
-        cin >> a[i];
+    //array input
+    for(i=0;i<n;i++){
+        cin>>a[i];
     }
 
-    for (i = 0; i <= n - 2; i++)
-    {
-        min = i;
-        for (j = i; j <= n - 1; j++)
-        {
-            if (a[j] < a[min])
-            {
-                min = j;
+    //Main Logic
+    for(i=0;i<=n-2;i++){
+        int min=i;
+        for(j=i;j<=n-1;j++){
+            if(a[j]<a[min]){
+                min=j;
             }
         }
-        temp = a[min];
-        a[min] = a[i];
-        a[i] = temp;
+        //swap
+        int temp= a[min];
+        a[min]= a[i];
+        a[i]=temp;
     }
-    cout << "Sorted elements are" << endl;
-    for (i = 0; i < n; i++)
-    {
 
-        cout << a[i] << " ";
+    // array output
+    for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
     }
 }
